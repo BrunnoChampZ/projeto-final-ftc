7018a9607f3ce5975b7d8946f2d1122a0c370b6c
BrunnoChampz <brunno.marcio@hotmail.com>
2024-01-11 00:02:38 -0300

    adicionando filtro visao cidades

--- pages/03_🏙️_Cities.py
+++ pages/03_🏙️_Cities.py
@@ -6,18 +6,29 @@ def make_sidebar(df):
     """ Esta função cria a sidebar da página"""
     st.sidebar.markdown("## Filtros")
 
-    countries = st.sidebar.multiselect(
-        "Escolha os Paises que Deseja visualizar as Informações",
-        df.loc[:, "country"].unique().tolist(),
-        default=["Brazil", "United Kingdom", "India", "United States", "Turkey", "Australia"],
-    )
+    all_countries = ["Todos"] + df.loc[:, "country"].unique().tolist()
+
+    container = st.sidebar.container()
+    select_all = container.checkbox("Selecionar todos os países")
+
+    if select_all:
+        selected_countries = container.multiselect(
+            "Escolha os Paises que Deseja visualizar as Informações:",
+            all_countries,
+            default=all_countries,
+        )
+    else:
+        selected_countries = container.multiselect(
+            "Escolha os Paises que Deseja visualizar as Informações:",
+            all_countries[1:],  # Exclui "Todos" da lista
+            default=["Brazil", "United Kingdom", "India", "United States", "Turkey", "Australia"],
+        )
 
     selected_palette = st.sidebar.selectbox(
-        "Escolha a paleta de cores dos gráficos",
-        px.colors.named_colorscales(), index=4
+        "Escolha a paleta de cores dos gráficos", px.colors.named_colorscales(), index=4
     )
 
-    return list(countries), selected_palette
+    return selected_countries, selected_palette
 
 
 def main():
@@ -26,27 +37,27 @@ def main():
 
     df = cdt.read_processed_data()
 
-    countries,selected_palette = make_sidebar(df)
+    selected_countries,selected_palette = make_sidebar(df)
 
     st.markdown("# :cityscape: Visão Cidades")
 
-    fig = cdt.top_cities_restaurants(countries, selected_palette)
+    fig = cdt.top_cities_restaurants(selected_countries, selected_palette)
 
     st.plotly_chart(fig, use_container_width=True)
 
     best, worst = st.columns(2)
 
     with best:
-        fig = cdt.top_best_restaurants(countries, selected_palette)
+        fig = cdt.top_best_restaurants(selected_countries, selected_palette)
 
         st.plotly_chart(fig, use_container_width=True)
 
     with worst:
-        fig = cdt.top_worst_restaurants(countries, selected_palette)
+        fig = cdt.top_worst_restaurants(selected_countries, selected_palette)
 
         st.plotly_chart(fig, use_container_width=True)
 
-    fig = cdt.most_cuisines(countries, selected_palette)
+    fig = cdt.most_cuisines(selected_countries, selected_palette)
 
     st.plotly_chart(fig, use_container_width=True)
 
