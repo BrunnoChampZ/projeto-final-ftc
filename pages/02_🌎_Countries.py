dd2fb300c38cfca7f6bd38867df5ae1aaffc49f0
BrunnoChampz <brunno.marcio@hotmail.com>
2024-01-10 23:42:48 -0300

    Adicionando filtro todos países

--- pages/02_🌎_Countries.py
+++ pages/02_🌎_Countries.py
@@ -6,17 +6,29 @@ def make_sidebar(df):
     """ Esta função cria a sidebar da página"""
     st.sidebar.markdown("## Filtros")
 
-    countries = st.sidebar.multiselect(
-        "Escolha os Paises que Deseja visualizar as Informações",
-        df.loc[:, "country"].unique().tolist(),
-        default=["Brazil", "United Kingdom", "India", "United States", "Turkey", "Australia"],
-    )
+    all_countries = ["Todos"] + df.loc[:, "country"].unique().tolist()
+
+    container = st.sidebar.container()
+    select_all = container.checkbox("Selecionar todos os países")
+
+    if select_all:
+        selected_countries = container.multiselect(
+            "Escolha os Paises que Deseja visualizar as Informações:",
+            all_countries,
+            default=all_countries,
+        )
+    else:
+        selected_countries = container.multiselect(
+            "Escolha os Paises que Deseja visualizar as Informações:",
+            all_countries[1:],  # Exclui "Todos" da lista
+            default=["Brazil", "United Kingdom", "India", "United States", "Turkey", "Australia"],
+        )
 
     selected_palette = st.sidebar.selectbox(
-        "Escolha a paleta de cores dos gráficos", px.colors.named_colorscales(), index = 4
+        "Escolha a paleta de cores dos gráficos", px.colors.named_colorscales(), index=4
     )
 
-    return list(countries), selected_palette
+    return selected_countries, selected_palette
 
 def main():
     """ Função principal da página"""
@@ -24,27 +36,27 @@ def main():
 
     df = cdt.read_processed_data()
 
-    countries, selected_palette = make_sidebar(df)
+    selected_countries, selected_palette = make_sidebar(df)
 
     st.markdown("# :earth_americas: Visão Países")
 
-    fig = cdt.restaurants_by_country(countries, selected_palette)
+    fig = cdt.restaurants_by_country(selected_countries, selected_palette)
 
     st.plotly_chart(fig, use_container_width=True)
 
-    fig = cdt.cities_by_country(countries, selected_palette)
+    fig = cdt.cities_by_country(selected_countries, selected_palette)
 
     st.plotly_chart(fig, use_container_width=True)
 
     votes, plate_price = st.columns(2)
 
     with votes:
-        fig = cdt.average_votes_per_country(countries, selected_palette)
+        fig = cdt.average_votes_per_country(selected_countries, selected_palette)
 
         st.plotly_chart(fig, use_container_width=True)
 
     with plate_price:
-        fig = cdt.average_cost_for_two_per_country(countries, selected_palette)
+        fig = cdt.average_cost_for_two_per_country(selected_countries, selected_palette)
 
         st.plotly_chart(fig, use_container_width=True)
 
